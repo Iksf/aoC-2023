8ac49b183aab76bdddf076455d8a9f4ab26d24fc
sporious@googlemail.com <sporious@googlemail.com>
2023-12-12 02:21:50 +0000

    shorten var name

--- src/util.h
+++ src/util.h
@@ -17,26 +17,26 @@ void read_file(const char *file_name, char **const buf);
 
 // atoi without needing null term string
 __attribute__((const)) inline long to_num_signed(const char *s, long n) {
-  long result = 0;
-  long sign = 1;
+  long res = 0;
+  long sig = 1;
   long i = 0;
   if (s[0] == '-') {
-    sign = -1;
+    sig = -1;
     i = 1;
   }
   for (; i < n && isdigit(s[i]); ++i)
-    result = result * 10 + (s[i] - '0');
+    res = res * 10 + (s[i] - '0');
 
-  return result * sign;
+  return res * sig;
 }
 
 __attribute__((const)) inline ulong to_num_unsigned(const char *s, long n) {
-  ulong result = 0;
+  ulong res = 0;
   long i = 0;
   for (; i < n && isdigit(s[i]); ++i)
-    result = result * 10 + (s[i] - '0');
+    res = res * 10 + (s[i] - '0');
 
-  return result;
+  return res;
 }
 
 #endif
